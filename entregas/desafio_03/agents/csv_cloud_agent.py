35d2d876e1c91c387dab00e1f35fa24a68142b3a
BerbardoMoschen <bernardomoschen.dev@gmail.com>
2025-06-16 12:09:34 -0300

    chore: envs

--- entregas/desafio_03/agents/csv_cloud_agent.py
+++ entregas/desafio_03/agents/csv_cloud_agent.py
@@ -28,7 +28,7 @@ class CSVAgent:
             f"Answer as clearly and accurately as possible."
         )
 
-        api_key = st.secrets["OPENROUTER_API_KEY"]
+        api_key = st.secrets["api_keys"]["openRouter"]
 
         headers = {
             "Authorization": f"Bearer {api_key}",
